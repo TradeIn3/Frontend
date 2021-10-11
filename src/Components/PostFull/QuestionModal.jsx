2a1e47235f671e35e85384d01d65b98307b69b6a
Sweeti-Gupta <sweetigupta945@gmail.com>
2021-10-11 11:43:02 +0530

    QuestionModal Breakpoint

--- src/Components/PostFull/QuestionModal.jsx
+++ src/Components/PostFull/QuestionModal.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Breakpoint } from "react-socks";
 import {
   Dialog,
   DialogTitle,
@@ -10,35 +11,72 @@ import {
 export default class QuestionModal extends Component {
   render() {
     return (
-      <Dialog
-        open={true}
-        onClose={this.handleClose}
-        aria-labelledby="form-dialog-title"
-        fullWidth
-        maxWidth="lg"
-      >
-        <DialogTitle className="title">
-          <h2>Ask your Question</h2>
-        </DialogTitle>
+      <React.Fragment>
+        <Breakpoint large up>
+          <Dialog
+            open={true}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+            fullWidth
+            maxWidth="lg"
+          >
+            <DialogTitle className="title">
+              <h2>Ask your Question</h2>
+            </DialogTitle>
 
-        <DialogContent
-          style={{ overflow: "hidden", height: "30rem", padding: "0" }}
-        >
-          <div className="qmodal">
-            <div className="qmodal__inner">
-              <h3>Write your question here...</h3>
-            </div>
-            <div className="qmodal__buttons">
-              <div className="qmodal__buttons__cancel">
-                <Button>Cancel</Button>
+            <DialogContent
+              style={{ overflow: "hidden", height: "30rem", padding: "0" }}
+            >
+              <div className="qmodal">
+                <div className="qmodal__inner">
+                  <h3>Write your question here...</h3>
+                </div>
+                <div className="qmodal__buttons">
+                  <div className="qmodal__buttons__cancel">
+                    <Button>Cancel</Button>
+                  </div>
+                  <div className="qmodal__buttons__save">
+                    <Button>Save</Button>
+                  </div>
+                </div>
               </div>
-              <div className="qmodal__buttons__save">
-                <Button>Save</Button>
+            </DialogContent>
+          </Dialog>
+        </Breakpoint>
+
+        <Breakpoint medium down>
+          <Dialog
+            open={true}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+            fullWidth
+            fullScreen
+            maxWidth="lg"
+          >
+            <DialogTitle className="title">
+              <h2>Ask your Question</h2>
+            </DialogTitle>
+
+            <DialogContent
+              style={{ overflow: "hidden", height: "30rem", padding: "0" }}
+            >
+              <div className="qmodal">
+                <div className="qmodal__inner">
+                  <h3>Write your question here...</h3>
+                </div>
+                <div className="qmodal__buttons">
+                  <div className="qmodal__buttons__cancel">
+                    <Button>Cancel</Button>
+                  </div>
+                  <div className="qmodal__buttons__save">
+                    <Button>Save</Button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
+            </DialogContent>
+          </Dialog>
+        </Breakpoint>
+      </React.Fragment>
     );
   }
 }
