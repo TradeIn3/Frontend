43b720ee97fe4e2c72efdfb413d187efd9d9eaff
Rohit Jain <rohit.jain@livehealth.in>
2024-04-14 15:53:03 +0530

    change origin to hostname

--- src/api/Request.js
+++ src/api/Request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_HOSTNAME = `${window.location.origin}:8000/api/`;
+const API_HOSTNAME = `${window.location.hostname}:8000/api/`;
 
 export const Request = async (method, endpoint, token, data) => {
 	return await axios({
