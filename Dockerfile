8ad72bd8a9b7c5a990c0803e1f64c74ccb3f8a3b
EC2 Default User <ec2-user@ip-172-31-40-91.ap-south-1.compute.internal>
2024-04-14 06:26:43 +0000

    Dockerfile changes

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:16.15.0 AS build
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+FROM nginx:1.17
+WORKDIR /usr/share/nginx/html
+COPY --from=build /app/build .
--- src/api/Request.js
+++ src/api/Request.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const API_HOSTNAME = "http://127.0.0.1:8000/api/";
-const API_HOSTNAME = window.location.origin;
+const API_HOSTNAME = `${window.location.origin}:8000/api/`;
 
 export const Request = async (method, endpoint, token, data) => {
 	return await axios({
